a61b684142766af64ec6a9440282f8fc3ff6dd7d
Samuel Magno <samuelrtfmagno@gmail.com>
2024-04-20 22:41:51 -0300

    Start GestaoVagas project'

---
+++ src/main/java/br/com/samuelmagno/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,22 @@
+package br.com.samuelmagno.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.samuelmagno.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("")
+    public void create(@Valid @RequestBody CandidateEntity candidate) {
+        System.out.println("Candidato:");
+        System.out.println(candidate.getEmail());
+    }
+}
