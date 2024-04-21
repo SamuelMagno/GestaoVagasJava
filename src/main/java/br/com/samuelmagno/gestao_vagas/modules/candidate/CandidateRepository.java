03cad7e1a2846b533360f73d6051794df3a56254
Samuel Magno <samuelrtfmagno@gmail.com>
2024-04-21 19:35:37 -0300

    candidate repository and validation

--- src/main/java/br/com/samuelmagno/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/samuelmagno/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -3,7 +3,9 @@ package br.com.samuelmagno.gestao_vagas.modules.candidate;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
-    
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
--- src/main/java/br/com/samuelmagno/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/samuelmagno/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,6 +3,7 @@ package br.com.samuelmagno.gestao_vagas.modules.candidate.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.samuelmagno.gestao_vagas.exceptions.UserFoundException;
 import br.com.samuelmagno.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.samuelmagno.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -19,10 +20,16 @@ public class CandidateController {
     @Autowired
     private CandidateRepository candidateRepository;
     
+    /**
+     * @param candidate
+     * @return
+     */
     @PostMapping("")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        System.out.println("Candidato:");
-        System.out.println(candidate.getEmail());
-        candidateRepository.save(candidate);
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
+        this.candidateRepository.findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
+        .ifPresent((user) -> {
+            throw new UserFoundException();
+        });
+        return candidateRepository.save(candidate);
     }
 }
